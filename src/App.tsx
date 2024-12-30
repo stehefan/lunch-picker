e2c23e81fa9c5d558c53d6b8009b73c83dc4d9cc
Stefan Lier <lier.stefan@gmail.com>
2024-12-30 21:36:07 +0100

    feat: add map of Hamburg to display lunch options

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import type { LatLngTuple } from 'leaflet'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import './App.css'
 
 function App() {
+  const position: LatLngTuple = [53.5502, 9.9920]
   return (
     <>
       <h1>Lunch Picker</h1>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </>
   )
 }
