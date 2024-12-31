00a2c619b33ffbcfcbfc8db49c3ad217fffd4043
Stefan Lier <lier.stefan@gmail.com>
2024-12-31 10:30:26 +0100

    fix: explicitly create markerIcon to bundle it properly when building

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import './App.css'
+
 import type { LatLngTuple } from 'leaflet'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
-import './App.css'
+import { Icon } from 'leaflet'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon1x from 'leaflet/dist/images/marker-icon.png';
 
 function App() {
   const position: LatLngTuple = [53.5502, 9.9920]
+  const markerIcon = new Icon({
+    iconUrl: markerIcon1x,
+    iconRetinaUrl: markerIcon2x,
+    iconSize: [25, 41],
+  });
+
   return (
     <>
       <h1>Lunch Picker</h1>
@@ -12,7 +22,7 @@ function App() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
+        <Marker position={position} icon={markerIcon}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
