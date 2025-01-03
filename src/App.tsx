9decc882d723995b36b42ec008ceb696cc0702b1
Stefan Lier <lier.stefan@gmail.com>
2025-01-03 14:23:24 +0100

    feat: display center point with image and make key parts configurable

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,42 @@
 import { Loader } from '@googlemaps/js-api-loader';
 import './App.css';
+import * as logo from '../public/logo.svg';
 
 function App() {
   const loader = new Loader({
     apiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY!,
     version: "weekly",
-    libraries: ["places"]
+    libraries: ["places"],
   });
-
+  const [lat, lng] = import.meta.env.VITE_MAPS_COORDINATES!.split(',');
+  const centerCoordinates = {
+    lat: parseFloat(lat),
+    lng: parseFloat(lng)
+  };
   const mapOptions = {
-    center: {
-      lat: 53.5502,
-      lng: 9.9920
-    },
-    zoom: 15
+    center: centerCoordinates,
+    zoom: parseInt(import.meta.env.VITE_MAPS_ZOOM || "15"),
+    mapId: import.meta.env.VITE_GOOGLE_MAPS_MAP_ID!
   };
 
-  loader
-    .importLibrary('maps')
-    .then(({ Map }) => {
-      new Map(document.getElementById("map") as HTMLElement, mapOptions);
-    })
-    .catch((e) => {
-      console.error(e);
+  Promise
+    .all([
+      loader.importLibrary("maps"),
+      loader.importLibrary("marker")
+    ])
+    .then(([mapsLibrary, markerLibrary]) => {
+      const mapElement: HTMLElement = document.getElementById('map')!;
+      const map = new mapsLibrary.Map(mapElement, mapOptions);
+
+      const logoElement = document.createElement('img');
+      logoElement.src = logo.default;
+
+      new markerLibrary.AdvancedMarkerElement({
+        position: centerCoordinates,
+        map: map,
+        title: "Center",
+        content: logoElement,
+      });
     });
 
   return (
