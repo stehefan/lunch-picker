a7b8d0c33b408ae92d4df8b9eefc885ad6428815
Stefan Lier <lier.stefan@gmail.com>
2025-01-10 11:34:47 +0100

    feat: add jotai devtools

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { useAtomsDebugValue } from 'jotai-devtools'
+
+// eslint-disable-next-line react-refresh/only-export-components
+const DebugAtoms = () => {
+  useAtomsDebugValue()
+  return null
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <DebugAtoms />
     <App />
   </StrictMode>,
 )
